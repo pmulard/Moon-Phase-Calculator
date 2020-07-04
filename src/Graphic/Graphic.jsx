6c934a2d41620c408dcd4e21fa6e4016aa30d607
pmully94 <peter.mulard@yahoo.com>
2020-07-03 22:51:38 -0700

    adds functions to format and set data into the HTML render

--- src/Graphic/Graphic.jsx
+++ src/Graphic/Graphic.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import './Graphic.css';
 
 import * as moonAlgorithms from '../Calculator/Algorithms.js';
-import * as calculator from '../Calculator/Calculator.jsx';
+import Calculator from '../Calculator/Calculator';
 
 export default class Graphic extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
+            moonImgs: {}
         };
 
         // this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
-        
+        this.setMoonImgs();
     }
 
     render() {
@@ -41,4 +42,63 @@ export default class Graphic extends React.Component {
             </div>
         )
     }
+
+    setMoonImgs = () => {
+        let moonImgs = {};
+    }
+
+    setData = () => {
+        const moon = Calculator.calculateMoon();
+        var date = moon.date,
+            latitude = moon.latitude,
+            longitude = moon.longitude,
+            location = this.formatLatLon(latitude, longitude),
+            riseTime = moon.riseTime,
+            setTime = moon.setTime,
+            phaseName = moon.phaseName,
+            phasePercent = moon.phasePercent;
+
+        this.setState(
+            {date},
+            {location},
+            {riseTime},
+            {setTime},
+            {phaseName},
+            {phasePercent}
+        );
+
+        document.getElementById("date").innerHTML = date;
+        document.getElementById("location").innerHTML = location;
+        document.getElementById("rise-time").innerHTML = riseTime;
+        document.getElementById("set-time").innerHTML = setTime;
+        document.getElementById("phase-name").innerHTML = phaseName;
+        document.getElementById("phase-percent").innerHTML = phasePercent;
+    
+    }
+
+    formatLatLon = (lat, lon) => {
+        let latitude = lat,
+            longitude = '';
+
+        // Latitude
+        if (lat < 0) {
+            latitude += '\xB0 S'
+        } else if (lat > 0) {
+            latitude += '\xB0 N'
+        } else { // Assertion: 0 degrees
+            latitude += '\xB0'
+        }
+
+        // Longitude
+        if (lon < 0) {
+            longitude += '\xB0 W'
+        } else if (lon > 0) {
+            longitude += '\xB0 E'
+        } else { // Assertion: 0 degrees
+            longitude += '\xB0'
+        }
+
+        return latitude + ' ' + longitude;
+    }
+
 }

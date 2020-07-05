81743215bbfbeca031986e931e079f1d24188317
pmully94 <peter.mulard@yahoo.com>
2020-07-05 12:29:56 -0700

    Reworked API to be based around current location. Adds map marker info window.

--- src/MapAPI/MapAPI.jsx
+++ src/MapAPI/MapAPI.jsx
@@ -1,32 +1,134 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import ReactDOM from 'react-dom';
 
-export class MapAPI extends React.Component {
+/*
+ * Class copied from https://www.digitalocean.com/community/tutorials/how-to-integrate-the-google-maps-api-into-react-applications
+*/
+
+const mapStyles = {
+    map: {
+      position: 'absolute',
+      width: '100%',
+      height: '100%'
+    }
+};
+  
+  export class CurrentLocation extends React.Component {
+    
     constructor(props) {
-        super(props)
-
+        super(props);
+    
+        const { lat, lng } = this.props.initialCenter;
         this.state = {
-            // array: []
+            currentLocation: {
+                lat: lat,
+                lng: lng
+            }
         };
     }
 
+    componentDidMount() {
+        if (this.props.centerAroundCurrentLocation) {
+            if (navigator && navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(pos => {
+                    const coords = pos.coords;
+                    this.setState({
+                        currentLocation: {
+                            lat: coords.latitude,
+                            lng: coords.longitude
+                        }
+                    });
+                });
+            }
+        }
+        this.loadMap();
+    }
+
+    loadMap() {
+        if (this.props && this.props.google) {
+            // checks if google is available
+            const { google } = this.props;
+            const maps = google.maps;
+        
+            const mapRef = this.refs.map;
+        
+            // reference to the actual DOM element
+            const node = ReactDOM.findDOMNode(mapRef);
+        
+            let { zoom } = this.props;
+            const { lat, lng } = this.state.currentLocation;
+            const center = new maps.LatLng(lat, lng);
+            const mapConfig = Object.assign(
+                {}, 
+                {
+                    center: center,
+                    zoom: zoom
+                }
+            );
+    
+          // maps.Map() is constructor that instantiates the map
+          this.map = new maps.Map(node, mapConfig);
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.google !== this.props.google) {
+          this.loadMap();
+        }
+        if (prevState.currentLocation !== this.state.currentLocation) {
+          this.recenterMap();
+        }
+    }
+
+    recenterMap() {
+        const map = this.map;
+        const current = this.state.currentLocation;
+    
+        const google = this.props.google;
+        const maps = google.maps;
+    
+        if (map) {
+            let center = new maps.LatLng(current.lat, current.lng);
+            map.panTo(center);
+        }
+    }
+
+    renderChildren() {
+        const { children } = this.props;
+    
+        if (!children) return;
+    
+        return React.Children.map(children, c => {
+            if (!c) return;
+            return React.cloneElement(c, {
+                map: this.map,
+                google: this.props.google,
+                mapCenter: this.state.currentLocation
+            });
+        });
+    }
+
     render() {
+        const style = Object.assign({}, mapStyles.map);
         return (
-            <Map
-            google={this.props.google}
-            zoom={12}
-            style={mapStyles}
-            initialCenter={{ lat: 37.773972, lng: -122.431297}}
-            />
+            <div>
+            <div style={style} ref="map">
+                Loading map...
+            </div>
+            {this.renderChildren()}
+            </div>
         );
     }
 }
 
-export default GoogleApiWrapper({
-    apiKey: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
-})(MapAPI);
-
-const mapStyles = {
-    width: '100%',
-    height: '100%',
+export default CurrentLocation;
+  
+CurrentLocation.defaultProps = {
+    zoom: 14,
+    initialCenter: {
+        lat: 42.1292,
+        lng: 80.0851
+    },
+    centerAroundCurrentLocation: false,
+    visible: true
 };

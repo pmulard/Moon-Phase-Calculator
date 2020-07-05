ee8fed1ddf952adcbcd5660ee696fd11c7901917
pmully94 <peter.mulard@yahoo.com>
2020-07-05 12:34:12 -0700

    fetches geolocation upon load. Adds maps functions, state objs and HTML

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import './Calculator.css';
 import Button from 'react-bootstrap/Button';
-import MapAPI from '../MapAPI/MapAPI';
+import { GoogleApiWrapper, InfoWindow, Marker  } from 'google-maps-react';
 import * as moonAlgorithms from './Algorithms.js';
+import CurrentLocation from '../MapAPI/MapAPI';
 
-export default class Calculator extends React.Component {
+export class Calculator extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            date: new Date(),
-            latitude: 37.7749,
-            longitude: 122.4194,
-            location: '',
-            riseTime: '',
-            setTime: '',
-            phaseName: '',
-            phasePercent: '',
+            date: {},
+            inputDate: {},
+            latitude: {},
+            longitude: {},
+            location: {},
+            riseTime: {},
+            setTime: {},
+            phaseName: {},
+            phasePercent: {},
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {}
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -24,7 +29,19 @@ export default class Calculator extends React.Component {
 
     // When app loads for the first time
     componentDidMount() {
-        
+        // Sets lat/lon state (and as a result, input fields) to current position
+        // Copied from: https://stackoverflow.com/questions/55829230/want-to-get-current-location-from-react-native-jsgoogle-api-key
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+              this.setState({
+                latitude: this.round(position.coords.latitude, 5),
+                longitude: this.round(position.coords.longitude, 5),
+                error: null,
+              });
+            },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 10000, maximumAge: 1000 },
+        );
     }
 
     clear() {
@@ -34,75 +51,76 @@ export default class Calculator extends React.Component {
     render() {
         return (
             <div className="app-container" class="app-container-fluid row justify-content-center">
-                <div className="block-container" class="col-sm-12 col-md-10 col-lg-8 d-flex justify-content-center d-flex flex-column">
+                <div className="block-container" class="col-sm-12 col-md-11 col-lg-10 d-flex justify-content-center d-flex flex-column">
                     <div class="graphic-container">
-                        <div className="app-container" class="app-container row justify-content-center d-flex flex-column">
-                    <div className="date-location-container" class="row justify-content-between">
-                        <div class="col-4" id="date-graphic"></div>
-                        <div class="col-4" id="location-graphic"></div>
-                    </div>
-                    <div className="rise-set-time-container" class="row">
-                        <div class="col-4" id="rise-time-graphic"></div>
-                        <div className="graphic-container" class="col-4">
-                            <img src="" alt="" id="moon-img-graphic"/>
+                        <div className="app-container" class="justify-content-center d-flex flex-column">
+                            <div className="date-location-container" class="row justify-content-between">
+                                <div class="graphic-element col-6" id="date-graphic">&nbsp;</div>
+                                <div class="graphic-element col-6" id="location-graphic">&nbsp;</div>
+                            </div>
+                            <div className="rise-set-time-container" class="row">
+                                <div class="graphic-element col-4" id="rise-time-graphic">&nbsp;</div>
+                                    <div className="graphic-container" class="col-4">
+                                        <img src="" alt="" id="moon-img-graphic"/>
+                                    </div>
+                                <div class="graphic-element col-4" id="set-time-graphic">&nbsp;</div>
+                            </div>
+                            <div className="phase-properties-container" class="row justify-content-center">
+                                <div class="graphic-element col-4">
+                                    <div id="phase-name-graphic">&nbsp;</div>
+                                    <div id="phase-percent-graphic">&nbsp;</div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-4" id="set-time-graphic"></div>
-                    </div>
-                    <div className="phase-properties-container" class="row justify-content-center">
-                        <div class="col-4">
-                            <div id="phase-name-graphic"></div>
-                            <div id="phase-percent-graphic"></div>
-                        </div>
-                    </div>
-                </div>
                     </div>  
                     <div className="form-container" class="form container-fluid d-flex flex-column">
                         <div className="date-container" class="form-group row">
-                            <label for="date-input" class="col-3 col-lg-2 col-form-label">Date</label>
-                            <div class="col-4">
+                            <label htmlFor="date-input" class="col-1 col-lg-2 col-form-label"></label>
+                            <div class="col-5 col-lg-4">
                                 <input class="form-control" type="date" id="date-input" placeholder="" name="inputDate" value={this.state.inputDate} onChange={this.handleChange}/>
                             </div>
                         </div>
                         <div className="location-input-container" class="form-group row">
-                            <label class="col-3 col-lg-2 col-form-label">Location</label>
-                            <div class="col-4">
+                            <label class="col-1 col-lg-2 col-form-label"></label>
+                            <div class="col-5 col-lg-4">
                                 <input class="form-control" type="number" placeholder="Latitude" id="location-input-latitude" name="latitude" value={this.state.latitude} onChange={this.handleChange}/>
                             </div>
-                            <div class="col-4">
+                            <div class="col-5 col-lg-4">
                                 <input class="form-control" type="number" placeholder="Longitude" id="location-input-longitude" name="longitude" value={this.state.longitude} onChange={this.handleChange}/>
                             </div>
                         </div>
                         <div className="location-button-container" class="form-group row">
-                            <div class="col-3 col-lg-2"></div>
-                            <div class="col-4 d-flex justify-content-start">
+                            <div class="col-1 col-lg-2"></div>
+                            <div class="col-5 col-lg-4 d-flex justify-content-start">
                                 <Button class="location-button">My Location</Button>
                             </div>
-                            <div class="col-4 d-flex justify-content-start " data-toggle="collapse" data-target="#map-api" aria-expanded="false" aria-controls="">
-                                <Button class="location-button">Google Maps</Button>
+                            <div class="col-5 col-lg-4 d-flex justify-content-start " data-toggle="collapse" data-target="#map-api" aria-expanded="false" aria-controls="">
+                                <Button class="location-button">Maps</Button>
                             </div>
                         </div>
                         <div className="google-maps-container" class="google-maps-container form-group row" id="google-maps-container">
-                            <div class="col-3 col-lg-2"></div>
+                            <div class="col-1 col-lg-2"></div>
                             <div class=" col-7 map-api">
-                                <MapAPI></MapAPI>
+                                <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
+                                    <Marker onClick={this.onMarkerClick} name={'current location'}/>
+                                    <InfoWindow
+                                        marker={this.state.activeMarker}
+                                        visible={this.state.showingInfoWindow}
+                                        onClose={this.onClose}
+                                        >
+                                        <div>
+                                            <h4>{this.state.selectedPlace.name}</h4>
+                                        </div>
+                                    </InfoWindow>
+                                </CurrentLocation>
                             </div>
-                            <div class="col-2 col-lg-auto"></div>
+                            <div class="col-1 col-lg-auto"></div>
                         </div>
-                        <div className="altitude-input-container" class="form-group row">
-                            <label class="col-3 col-lg-2 col-form-label">Altitude</label>
-                            <div class="input-group mb-3 col-6 col-lg-4">
-                                <input type="number" min="0" class="form-control" placeholder="0" name="altitude" value={this.state.altitude} onChange={this.handleChange}/>
-                                <div class="input-group-append">
-                                    <span class="input-group-text" id="basic-addon2">meters</span>
-                                </div>
-                            </div>
-                        </div>
-                        
                         <div className="execute-clear-container" class="form-group row">
                             <div class="col-1 col-lg-2">
                             </div>
                             <div class="col-7 col-lg-6">
-                                <button type="button" class="execute-button btn btn-primary btn-lg btn-block" onClick={this.calculateMoon}>Show me the moon-ey!</button>
+                                <button type="button" class="execute-button btn btn-primary btn-lg btn-block" onClick={this.calculateMoon}>Generate</button>
                             </div>
                             <div class="col-3 col-lg-2">
                                 <button type="button" class="execute-button btn btn-danger btn-lg btn-block">Clear</button>
@@ -112,17 +130,23 @@ export default class Calculator extends React.Component {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> 
         );
     }
+    
 
     // Handles state changes for local objects
-    handleChange = (e) => {
+    handleChange (e) {
         e.preventDefault();
         this.setState({ [e.target.name]: e.target.value });
     };
 
-    setDateState = () => {
+
+    /*
+     * MOON CALCULATION FUNCTIONS
+    */
+
+    async setDateState () {
         const inputDate = this.state.inputDate;
         let YY = parseInt(inputDate.substring(0,4)),
             MM = parseInt(inputDate.substring(5,7))-1,
@@ -133,34 +157,36 @@ export default class Calculator extends React.Component {
             ss = dateOBJ.getSeconds();
 
         const dateValue = new Date(YY, MM, DD, hh, mi, ss);
-        this.setState({date: dateValue});
+        await this.setState({date: dateValue});
+        await this.setMoonDataState();
+        this.setGraphic();
     }
 
-    setMoonDataState = () => {
+    setMoonDataState () {
         var date = this.state.date,
             lat = this.state.latitude,
-            lon = this.state.longitude;
+            lon = this.state.longitude,
+            riseTimeVal = '⭜' + moonAlgorithms.getMoonRiseTime(date, lat, lon),
+            setTimeVal = '⭝' + moonAlgorithms.getMoonSetTime(date, lat, lon)
         
         this.setState({
             location: this.formatLatLon(lat, lon),
-            riseTime: moonAlgorithms.getMoonRiseTime(date, lat, lon),
-            setTime: moonAlgorithms.getMoonSetTime(date, lat, lon),
+            riseTime: riseTimeVal.slice(0,1) + " " + riseTimeVal.slice(1),
+            setTime: setTimeVal.slice(0,1) + " " + setTimeVal.slice(1),
             phaseName: 'Sample Name',
             phasePercent: this.getPhasePercent()
-        })
+        }, this.setGraphic());
     }
 
     calculateMoon = () => {
-            this.setDateState();
-            this.setMoonDataState();
-            this.setGraphic();
+        this.setDateState();
     };
 
     setGraphic = () => {
-        document.getElementById("date-graphic").innerHTML = this.state.date;
+        document.getElementById("date-graphic").innerHTML = this.state.date.toString().slice(0,15);
         document.getElementById("location-graphic").innerHTML = this.state.location;
         document.getElementById("rise-time-graphic").innerHTML = this.state.riseTime;
-        document.getElementById("set-time-graphic").innerHTML = `${this.state.setTime}`;
+        document.getElementById("set-time-graphic").innerHTML = this.state.setTime;
         document.getElementById("phase-name-graphic").innerHTML = this.state.phaseName;
         document.getElementById("phase-percent-graphic").innerHTML = this.state.phasePercent;
     }
@@ -179,6 +205,11 @@ export default class Calculator extends React.Component {
         return (this.round(fraction, 4)*100 + '%');
     };
 
+
+    /*
+     * UTILITY FUNCTIONS
+    */
+
     // Copied from https://www.jacklmoore.com/notes/rounding-in-javascript/
     round = (value, decimals) => {
         return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
@@ -190,55 +221,48 @@ export default class Calculator extends React.Component {
 
         // Latitude
         if (lat < 0) {
-            latitude += '\xB0 S'
+            latitude = Math.abs(lat) + '\xB0 S';
         } else if (lat > 0) {
-            latitude += '\xB0 N'
+            latitude += '\xB0 N';
         } else { // Assertion: 0 degrees
-            latitude += '\xB0'
+            latitude += '\xB0';
         }
 
         // Longitude
         if (lon < 0) {
-            longitude += '\xB0 W'
+            longitude = Math.abs(lat) + '\xB0 W';
         } else if (lon > 0) {
-            longitude += '\xB0 E'
+            longitude += '\xB0 E';
         } else { // Assertion: 0 degrees
-            longitude += '\xB0'
+            longitude += '\xB0';
         }
 
-        return latitude + ' ' + longitude;
-    }
+        return latitude + ', ' + longitude;
+    };
 
-    
+
+    /*
+     * GOOGLE MAPS FUNCTIONS
+    */
+
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+    });
+
+    onClose = (props) => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            });
+        }
+    };
 }
 
-// Retrieving the moon objects that hold data
-            // const moonIllum = moonAlgorithms.getMoonIllumination(date);
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyDyFJ8p4lyEWfpaE9KEOE93iTAjO6X0sOE'
+})(Calculator);
 
-            /*
-            * Moon Fraction - illuminated fraction of the moon;
-            * varies from 0.0 (new moon) to 1.0 (full moon)
-            */
-            // const fraction = moonIllum.fraction;
-
-            /*
-            *  Moon Phase - moon phase; varies from 0.0 to 1.0:
-            *      0	New Moon
-            *          Waxing Crescent
-            *   0.25	First Quarter
-            *          Waxing Gibbous
-            *    0.5	Full Moon
-            *          Waning Gibbous
-            *   0.75	Last Quarter
-            *          Waning Crescent
-            */
-            // const phase = moonIllum.phase;
-
-            /*
-            * Moon Angle - midpoint angle in radians of the 
-            * illuminated limb of the moon reckoned eastward 
-            * from the north point of the disk; the moon is 
-            * waxing if the angle is negative, and waning if 
-            * positive
-            */
-            // const angle = moonIllum.angle;
